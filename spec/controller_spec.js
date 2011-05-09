8fd99849c3d2b64721b5673264d3c236b7c99789
Guy Royse <guy@guyroyse.com>
2011-05-08 22:08:28 -0400

    Controler under spec

--- spec/controller_spec.js
+++ spec/controller_spec.js
@@ -1,9 +1,35 @@
-var controller = require('../lib/controller')
+describe("Controller", function() {
 
-describe("controller", function() {
+	var TEST_USER = 'test';
+	var TEST_PASSWORD = 'password';
 
-	it("runs", function() {
-		expect(2+2).toEqual(4);
+	var controller;
+
+	beforeEach(function() {
+		controller = require('../lib/controller')
 	});
+
+	it("asks for a user when started", function() {
+		expect(controller.start()).toEqual("User: ");
+	});
+
+	it("asks for a password when a user is entered", function() {
+		controller.start();
+		expect(controller.next(TEST_USER)).toEqual("Password: ");
+	});
+
+	it("displays the welcome message when user and password are good", function() {
+		controller.authn = { authenticate : function() { return true; } };
+		controller.start();
+		controller.next(TEST_USER);
+		expect(controller.next(TEST_PASSWORD)).toEqual("Welcome to the server\r\n");
+	});
+
+	it("displays an error message and asks for a new user when user and password are bad", function() {
+		controller.authn = { authenticate : function() { return false; } };
+		controller.start();
+		controller.next(TEST_USER);
+		expect(controller.next(TEST_PASSWORD)).toEqual("Invalid login\r\nUser: ");
+	}); 
 
 });
